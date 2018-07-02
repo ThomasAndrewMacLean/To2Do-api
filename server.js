73cc92b6af9913b50519fb4669e2db271ffe1651
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2018-07-03 01:46:53 +0200

    added dummy routes to test database

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require('express');
 var cookieParser = require('cookie-parser')
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const db = require('monk')(`mongodb://dbreadwright:${process.env.MONGO_PW}@ds018708.mlab.com:18708/to2so`)
+const db = require('monk')(`mongodb://dbreadwrite:${process.env.MONGO_PW}@ds018708.mlab.com:18708/to2so`)
 
 const app = express();
 
@@ -18,10 +18,31 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+let users = db.get('users');
+
+app.get('/users', (req, res) => {
+    users.find().then(d => res.status(200).json(d));
+})
+
+app.post('/users', (req, res) => {
+    users.insert({
+        email: req.body.user
+    }).then(r => res.status(200).json(r))
+})
+
+
+
 app.get('/test', (req, res) => {
     res.status(200).json({
         'message': 'hello world!'
     })
 })
+
+app.get('*/*', (req, res) => {
+    res.status(200).json({
+        'message': '!!!'
+    })
+})
+
 
 app.listen(process.env.PORT || 5001, () => console.log('All is ok, sit back and relax!'));

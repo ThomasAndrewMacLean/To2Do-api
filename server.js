7c33fe2681681cf772b858ba8a4cf08c0ba92c29
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2018-07-03 08:26:38 +0200

    jwt

--- server.js
+++ server.js
@@ -6,13 +6,14 @@ const express = require('express');
 var cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
 const db = require('monk')(`mongodb://dbreadwrite:${process.env.MONGO_PW}@ds018708.mlab.com:18708/to2so`);
 
 const app = express();
 
 
 app.use(cookieParser());
-app.use(morgan('tiny'));
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
@@ -21,13 +22,23 @@ app.use(bodyParser.urlencoded({
 let users = db.get('users');
 
 app.get('/users', (req, res) => {
+    console.log(req);
+
     users.find().then(d => res.status(200).json(d));
 });
 
-app.post('/users', (req, res) => {
-    users.insert({
-        email: req.body.user
-    }).then(r => res.status(200).json(r));
+app.post('/users', verifyToken, (req, res) => {
+    jwt.verify(req.token, 'secretkey', (err, authData) => {
+        if (err) {
+            res.sendStatus(403);
+        } else {
+            users.insert({
+                authData,
+                email: req.body.user
+            }).then(r => res.status(200).json(r));
+        }
+    });
+
 });
 
 app.delete('/users', (req, res) => {
@@ -37,6 +48,24 @@ app.delete('/users', (req, res) => {
 });
 
 
+app.post('/login', (req, res) => {
+    // Mock user
+    const user = {
+        id: 1,
+        username: 'Thomas',
+        email: 'thomas.maclean@gmail.com'
+    };
+
+    jwt.sign({
+        user
+    }, 'secretkey', {
+        expiresIn: '30s'
+    }, (err, token) => {
+        res.json({
+            token
+        });
+    });
+});
 app.get('/test', (req, res) => {
     res.status(200).json({
         'message': 'hello world!'
@@ -49,5 +78,29 @@ app.get('*/*', (req, res) => {
     });
 });
 
+
+// FORMAT OF TOKEN
+// Authorization: Bearer <access_token>
+
+// Verify Token
+function verifyToken(req, res, next) {
+    // Get auth header value
+    const bearerHeader = req.headers['authorization'];
+    // Check if bearer is undefined
+    if (typeof bearerHeader !== 'undefined') {
+        // Split at the space
+        const bearer = bearerHeader.split(' ');
+        // Get token from array
+        const bearerToken = bearer[1];
+        // Set the token
+        req.token = bearerToken;
+        // Next middleware
+        next();
+    } else {
+        // Forbidden
+        res.sendStatus(403);
+    }
+
+}
 
 app.listen(process.env.PORT || 5001, () => console.log('All is ok, sit back and relax!'));

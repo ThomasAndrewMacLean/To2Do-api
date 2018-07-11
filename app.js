89e2bf37939b3a05e48b211a485f139eeb4dcb37
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2018-07-11 22:24:40 +0200

    f

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ let fs = require('fs');
 const sendMail = (mail, linky) => {
     console.log('start mail');
 
-    let data = fs.readFileSync('./to2doSignUpMail/mail.html', 'utf8');
+    let data = fs.readFileSync('./views/mail.html', 'utf8');
     console.log(data);
     mailOptions.html = data.replace('{{{link}}}', linky);
     mailOptions.to = mail;

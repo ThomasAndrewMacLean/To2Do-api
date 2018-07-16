9bc4d03ad84c7dfbb806894d34a1f1ce370a69cc
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2018-07-16 20:01:47 +0200

    rollup

---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+import babel from 'rollup-plugin-babel';
+import babelrc from 'babelrc-rollup';
+
+export default {
+    output: {
+        file: 'dist/server_production.js',
+        format: 'cjs'
+    },
+    entry: 'src/server.js',
+    dest: 'dist/server_production.js',
+    plugins: [
+        babel(babelrc())
+    ]
+};

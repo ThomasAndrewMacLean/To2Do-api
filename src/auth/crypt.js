ebf09d47e46583013fa7cd115985f172ae1ed4c5
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2018-08-26 12:14:21 +0200

    try catch 🎎

--- src/auth/crypt.js
+++ src/auth/crypt.js
@@ -2,18 +2,22 @@ const crypto = require('crypto');
 const algorithm = 'aes-256-ctr';
 const password = process.env.CRYPTO;
 
-export let encrypt = (text) => {
+export let encrypt = text => {
     var cipher = crypto.createCipher(algorithm, password);
     var crypted = cipher.update(text, 'utf8', 'hex');
     crypted += cipher.final('hex');
     return crypted;
-
 };
-export let decrypt = (text) => {
-    var decipher = crypto.createDecipher(algorithm, password);
-    var dec = decipher.update(text, 'hex', 'utf8');
-    dec += decipher.final('utf8');
-    return dec;
+export let decrypt = text => {
+    try {
+        var decipher = crypto.createDecipher(algorithm, password);
+        var dec = decipher.update(text, 'hex', 'utf8');
+        dec += decipher.final('utf8');
+        return dec;
+    } catch (error) {
+        console.log(error);
+        return text;
+    }
 };
 
 export default crypto;
